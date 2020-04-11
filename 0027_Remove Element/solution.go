1e5d485f64a38be3579754ca89873558ce95cd69
hi0t <27971344+hi0t@users.noreply.github.com>
2020-04-11 21:06:16 +0300

    couple solutions

---
+++ 0027_Remove Element/solution.go
@@ -0,0 +1,18 @@
+package solution
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	j := 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[j] != nums[i] {
+			j++
+			nums[j] = nums[i]
+		}
+	}
+
+	return j + 1
+}
